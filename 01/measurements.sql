3c6a4f4cbeea1ba98530b4317286399163a99bc3
Vikke Tiirola <vikke.tiirola@gmail.com>
2021-12-01 13:28:55 +0200

    Add 01-2

--- 01/measurements.sql
+++ 01/measurements.sql
@@ -16,21 +16,29 @@ create table aoc_01 (value int);
 --     (260),
 --     (263);
 
-\copy aoc_01 from 'input' 
+\copy aoc_01 from 'input'
 
 with compared_data as (
     select
         value,
-        lag(value) over() as prev
+        lag(value) over() as prev1,
+        lag(value, 2) over() as prev2,
+        lag(value, 3) over() as prev3
     from
         aoc_01
 )
 select
     sum(
         case
-            when value > prev then 1
+            when value > prev1 then 1
             else 0
         end
-    )
+    ) as part1,
+    sum(
+        case
+            when (value + prev1 + prev2) > (prev1 + prev2 + prev3) then 1
+            else 0
+        end
+    ) as part2
 from
     compared_data;
